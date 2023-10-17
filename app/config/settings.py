c8b86ba50ed561c066e7c26450f8ec39f4339a69
Richard Y. Hayashi <rhayashi@gruuw.com>
2023-10-17 21:20:43 +0900

    Added to env & package setup

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,13 +1,16 @@
 """
     config.settings.py
 """
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SecretStr, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    db_dialect: str = "mysql"
     db_host: str = "localhost"
     db_port: int = 3306
     db_name: str = ""
+    db_username: str = ""
+    db_password: SecretStr = ""
 
     model_config = SettingsConfigDict(
                         case_sensitive = False,
